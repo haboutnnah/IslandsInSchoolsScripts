521058811be910d9e015145834727d341984da45
Hannah Ivy <contact@hannahi.com>
2018-09-13 09:51:41 +1000

    Handle 0 residents.

--- Check Number of Participants.user.js
+++ Check Number of Participants.user.js
@@ -10,6 +10,7 @@
 (function() {
     'use strict'; // Forces the code to be neat
     var num_residents = parseInt(document.getElementById("t1").children[0].innerHTML.replace("You have ","").replace(" contacts.","")) // Gets the number of residents
+    if (document.getElementById("t1").children[0].innerHTML == "Your contacts list is empty. Visit an Islander to add them."){num_residents = 0} // Handle 0 residents
     console.log("We have " + num_residents + " residents so far") // Chucks it in to the log for debugging
     if (num_residents < 40){ // If it's less than forty
         window.location.href = "http://islands.smp.uq.edu.au/village.php?Pauma" // Go get another!
