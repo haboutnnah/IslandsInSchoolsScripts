14e0b238d96f2fe93ba5ca6e8477b6fd214313c0
Hannah Ivy <contact@hannahi.com>
2018-09-12 19:34:06 +1000

    Add min and max age variables

--- Get Consent.user.js
+++ Get Consent.user.js
@@ -7,15 +7,15 @@
 // @match        http://islands.smp.uq.edu.au/islande*
 // ==/UserScript==
 
+var min_age = console.log("Do the setup ._.") // 25
+var max_age = console.log("Do the setup ._.") // 35
+
+
 (function() {
     'use strict';
     var age = parseInt((document.getElementsByClassName("storyevent")[1]).innerText.replace(" years old",""));
 
-    console.log("Age found: ", age)
-    if ((age >= 25) && (age <= 35)){
-        change_tab('t3')
-        getConsent(window.location.href.replace(/.*?=/,""))
-        window.location.href = "http://islands.smp.uq.edu.au/project.php"
+    if ((age >= min_age) && (age <= max_age)){ // If in age range
     }
     else{
         window.location.href = "http://islands.smp.uq.edu.au/project.php"
