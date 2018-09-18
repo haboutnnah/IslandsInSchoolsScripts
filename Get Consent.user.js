cbee3486dc180da5595293ee767a84c735daa549
Hannah Ivy <contact@hannahi.com>
2018-09-18 17:52:39 +1000

    Abstract away setting age

--- Get Consent.user.js
+++ Get Consent.user.js
@@ -16,24 +16,24 @@
     'use strict';
     var age = parseInt((document.getElementsByClassName("storyevent")[1]).innerText.replace(" years old","")); // Get the age
 
+    var set_age = async(variable_name, description) => {
+        let before = await GM.getValue(variable_name, "nil"); // Get how many times we've clicked a resident, if none, set to zero.
+        if (before == "nil"){ before = parseInt(prompt("What is the " + description + " age in your age group?"))}
+        await GM.setValue(variable_name, before); // Add one
+        return await GM.getValue(variable_name); // Now do it to the local value    
+    }
+
     let count_before = await GM.getValue('count', 0); // Get how many times we've clicked a resident, if none, set to zero.
     await GM.setValue('count', count_before + 1); // Add one
     let count_after = await GM.getValue('count'); // Now do it to the local value
+
     console.log("We've clicked on", count_after, "islanders."); // Print it to console
     console.log("Age found: ", age) // Dump it to console
 
-    let min_age = await GM.getValue('min_age', "nil"); // Get how many times we've clicked a resident, if none, set to zero.
-    if (min_age == "nil"){ min_age = parseInt(prompt("What is the minimum age in your age group?"))}
-    await GM.setValue('min_age', min_age); // Add one
-    let new_min_age = await GM.getValue('min_age'); // Now do it to the local value
+    var min_age = set_age("min_age", "minimum")
+    var max_age = set_age("max_age", "maximum")
 
-    let max_age = await GM.getValue('max_age', "nil"); // Get how many times we've clicked a resident, if none, set to zero.
-    if (max_age == "nil"){ max_age = parseInt(prompt("What is the minimum age in your age group?"))}
-    await GM.setValue('max_age', max_age); // Add one
-    let new_max_age = await GM.getValue('max_age'); // Now do it to the local value
-
-
-    if ((age >= new_min_age) && (age <= new_max_age)){ // If in age range
+    if ((age >= min_age) && (age <= max_age)){ // If in age range
         change_tab('t3') // Change to consent tab
         getConsent(window.location.href.replace(/.*?=/,"")) // Get the islander ID
         window.location.href = "http://islands.smp.uq.edu.au/project.php" // Go check how many residents we have
