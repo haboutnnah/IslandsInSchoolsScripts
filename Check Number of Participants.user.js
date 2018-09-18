803ad97fce1f6ba56972336882dfe351c739d7b9
Hannah Ivy <contact@hannahi.com>
2018-09-18 16:24:20 +1000

    Up number to 80.

--- Check Number of Participants.user.js
+++ Check Number of Participants.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Check Number of Participants
-// @description  Checks number of participants, if less than 40, continue.
+// @description  Checks number of participants, if less than 80, continue.
 // @version      0.1
 // @namespace    https://hannahi.com/
 // @author       Hannah Ivy <contact@hannahi.com>
@@ -13,7 +13,7 @@
     var num_residents = parseInt(document.getElementById("t1").children[0].innerHTML.replace("You have ","").replace(" contact.", "").replace(" contacts.","")) // Gets the number of residents
     if (document.getElementById("t1").children[0].innerHTML == "Your contacts list is empty. Visit an Islander to add them."){num_residents = 0} // Handle 0 residents
     console.log("We have " + num_residents + " residents so far") // Chucks it in to the log for debugging
-    if (num_residents < 40){ // If it's less than forty
+    if (num_residents < 80){ // If it's less than eighty
         window.location.href = "http://islands.smp.uq.edu.au/village.php?Pauma" // Go get another!
     }
 })();
